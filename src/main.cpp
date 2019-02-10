e1c6d0719fb049cd652a8135fa83a71e834e4894
Senfo <lpsenfo@web.de>
2019-02-10 01:59:04 +0100

    indent

--- src/main.cpp
+++ src/main.cpp
@@ -94,20 +94,20 @@ void extensionDeInit() {
 #include <Windows.h>
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
-    case DLL_PROCESS_ATTACH: {
-        extensionInit();
-        break;
-    }
-    case DLL_THREAD_ATTACH: {
-        break;
-    }
-    case DLL_THREAD_DETACH: {
-        break;
-    }
-    case DLL_PROCESS_DETACH: {
-        extensionDeInit();
-        break;
-    }
+        case DLL_PROCESS_ATTACH: {
+            extensionInit();
+            break;
+        }
+        case DLL_THREAD_ATTACH: {
+            break;
+        }
+        case DLL_THREAD_DETACH: {
+            break;
+        }
+        case DLL_PROCESS_DETACH: {
+            extensionDeInit();
+            break;
+        }
     }
 
     return TRUE;
